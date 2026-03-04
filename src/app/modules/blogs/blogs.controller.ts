fb660af5936b8e15c5b8bce6d8d591bfdf6c686a
La Bao Chien <4801104018@student.hcmue.edu.vn>
2026-03-04 14:04:28 +0700

    refactor(blogs): improve method naming and update response handling in blogs controller

--- src/app/modules/blogs/blogs.controller.ts
+++ src/app/modules/blogs/blogs.controller.ts
@@ -3,7 +3,7 @@ import { BlogsService } from './blogs.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
 import { PaginationDto } from '../pagination/pagination.dto';
-import { ok } from 'assert';
+import { ok } from 'src/app/common/response/api-response';
 
 @Controller('blogs')
 export class BlogsController {
@@ -20,18 +20,18 @@ export class BlogsController {
   }
 
   @Get('view/:id')
-  findOne(@Param('id') id: string) {
+  findBlogById(@Param('id') id: string) {
     return this.blogsService.findBlogById(id);  
   }
 
   @Put('update/:id')
-  update(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDto) {
+  updateBlog(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDto) {
     return this.blogsService.updateBlog(id, updateBlogDto);
   }
 
   @Delete('delete/:id')
-  remove(@Param('id') id: string) {
+  deleteBlog(@Param('id') id: string) {
     const result = this.blogsService.deleteBlog(id);
-    return ok(result, 'Blog deleted successfully');
+    return ok(result, 'Blog deleted successfully', 200);
   }
 }
