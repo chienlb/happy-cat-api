1e77419b033f677e8c32bbe082c8e3f403c1bcfc
La Bảo Chiến <4801104018@student.hcmue.edu.vn>
2026-03-22 03:02:16 +0700

    feat(communites): refactor CreateCommuniteDto to use CreateCommentDto for comments

--- src/app/modules/communites/dto/create-communite.dto.ts
+++ src/app/modules/communites/dto/create-communite.dto.ts
@@ -1,12 +1,43 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsBoolean,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+class CreateCommentDto {
+  @IsString()
+  userId: string;
+
+  @IsString()
+  content: string;
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+}
 
 export class CreateCommuniteDto {
+  @IsOptional()
+  @IsString()
   userId: string;
+
+  @IsString()
   content: string;
+
+  @IsOptional()
+  @IsString()
   image?: string;
-  comments: {
-    userId: string;
-    content: string;
-    image?: string;
-  }[];
-  isPublic: boolean;
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateCommentDto)
+  comments?: CreateCommentDto[];
+
+  @IsOptional()
+  @IsBoolean()
+  isPublic?: boolean;
 }
